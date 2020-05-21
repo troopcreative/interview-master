e38a3bd58657c84303060ccc2c6df426c43875da
Luke Bennett <contact@stacki.ltd>
2020-05-21 18:37:55 +0100

    Add rating hover

--- src/Rating.js
+++ src/Rating.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Rating({ rating = 0, maxRating = 5, onRate = () => {} }) {
+  const [hoveredRating, setHoveredRating] = useState(false);
+
   const generateRating = () => {
     let finalRating = [];
     for (let x = 0; x < maxRating; x += 1) {
@@ -8,15 +10,23 @@ function Rating({ rating = 0, maxRating = 5, onRate = () => {} }) {
       finalRating.push(
         <div
           key={x}
-          className={`rating-pip ${normalRating <= rating ? "active" : ""}`}
+          className={`rating-pip ${normalRating <= (hoveredRating || rating) ? "active" : ""}`}
           onClick={() => onRate(normalRating)}
+          onMouseEnter={() => setHoveredRating(normalRating)}
         />
       );
     }
     return finalRating;
   };
 
-  return <div className="rating">{generateRating()}</div>;
+  return (
+    <div
+      className="rating"
+      onMouseLeave={() => setHoveredRating(undefined)}
+    >
+      {generateRating()}
+    </div>
+  );
 }
 
 export default Rating;
